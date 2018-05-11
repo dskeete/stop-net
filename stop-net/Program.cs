d72c8a1c7f01ad1a22b4857941aa2d158c4c5b40
dskeete <dskeete@artnet.com>
2018-05-11 16:04:52 -0400

    Add stop-net solution and project

---
+++ stop-net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stop_net
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Stop!");
+        }
+    }
+}
