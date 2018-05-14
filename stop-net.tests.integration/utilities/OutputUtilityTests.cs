4fc37ee58825037ff5d2e5bf18ebb8362fc71677
dskeete <dskeete@artnet.com>
2018-05-14 19:03:51 -0400

    Added specflow test

---
+++ stop-net.tests.integration/utilities/OutputUtilityTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stop_net.utilities;
+
+namespace stop_net.tests.integration.utilities
+{
+    [TestClass]
+    public class OutputUtilityTests
+    {
+        [TestMethod]
+        public void WriteLineReturnsCorrectOutput()
+        {
+            OutputUtility.Instance.WriteLine().Should().Be("Stop!");
+        }
+    }
+}
--- stop-net.utilities/OutputUtility.cs
+++ stop-net.utilities/OutputUtility.cs
@@ -4,7 +4,7 @@ namespace stop_net.utilities
 {
     public class OutputUtility
     {
-        public static OutputUtility Instance { get; }
+        public static OutputUtility Instance { get { return Implementation; } }
         public static OutputUtility Implementation = new OutputUtility();
 
         private OutputUtility() { }
