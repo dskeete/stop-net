a8c8175e0c0c0ad266f24eff8322c0841f8db3de
dskeete <dskeete@artnet.com>
2018-05-15 11:14:41 -0400

    remove specflow. msbuild on mono

--- stop-net.tests.integration/OutputSteps.cs
+++ stop-net.tests.integration/OutputSteps.cs
@@ -1,41 +1,41 @@
-﻿using FluentAssertions;
-using System;
-using System.Diagnostics;
-using TechTalk.SpecFlow;
+﻿//using FluentAssertions;
+//using System;
+//using System.Diagnostics;
+//using TechTalk.SpecFlow;
 
-namespace stop_net.tests.integration
-{
-    [Binding]
-    public class OutputSteps
-    {
-        public Process _proc { get; private set; }
+//namespace stop_net.tests.integration
+//{
+//    [Binding]
+//    public class OutputSteps
+//    {
+//        public Process _proc { get; private set; }
 
-        [Given(@"an output process")]
-        public void GivenAnOutputProcess()
-        {
-            _proc = new Process();
-            _proc.StartInfo.FileName = @"dotnet";
-            _proc.StartInfo.Arguments = @"..\..\..\..\stop-net\bin\Debug\netcoreapp2.0\stop-net.dll";
-            _proc.StartInfo.UseShellExecute = false;
-            _proc.StartInfo.RedirectStandardOutput = true;
-        }
+//        [Given(@"an output process")]
+//        public void GivenAnOutputProcess()
+//        {
+//            _proc = new Process();
+//            _proc.StartInfo.FileName = @"dotnet";
+//            _proc.StartInfo.Arguments = @"..\..\..\..\stop-net\bin\Debug\netcoreapp2.0\stop-net.dll";
+//            _proc.StartInfo.UseShellExecute = false;
+//            _proc.StartInfo.RedirectStandardOutput = true;
+//        }
 
-        [When(@"the output process is executed")]
-        public void WhenTheOutputProcessIsExecuted()
-        {
-            _proc.Start();
-        }
+//        [When(@"the output process is executed")]
+//        public void WhenTheOutputProcessIsExecuted()
+//        {
+//            _proc.Start();
+//        }
         
-        [Then(@"the output is valid")]
-        public void ThenTheOutputIsValid()
-        {
-            string outPut = _proc.StandardOutput.ReadToEnd();
+//        [Then(@"the output is valid")]
+//        public void ThenTheOutputIsValid()
+//        {
+//            string outPut = _proc.StandardOutput.ReadToEnd();
 
-            _proc.WaitForExit();
-            var exitCode = _proc.ExitCode;
-            _proc.Close();
+//            _proc.WaitForExit();
+//            var exitCode = _proc.ExitCode;
+//            _proc.Close();
 
-            outPut.Should().Be("Stop!" + "\r\n");
-        }
-    }
-}
+//            outPut.Should().Be("Stop!" + "\r\n");
+//        }
+//    }
+//}
