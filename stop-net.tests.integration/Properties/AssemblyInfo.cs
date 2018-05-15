1518b3ec8304862ad6bfca26a1a77520e99249b5
dskeete <dskeete@artnet.com>
2018-05-15 11:42:27 -0400

    Add net461 test project

---
+++ stop-net.tests.integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("stop-net.tests.integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("stop-net.tests.integration")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b57fe58-86c1-4a9a-88b3-afd50a1b7693")]
---
+++ stop-net.tests.integration/StepDefinition.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace stop_net.tests.integration
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"I am curious")]
+        public void GivenIAmCurious()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I request the version")]
+        public void WhenIRequestTheVersion()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I yell '(.*)'")]
+        public void WhenIYell(string exclamation)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result is content")]
+        public void ThenTheResultIsContent()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result is constant")]
+        public void ThenTheResultIsConstant()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I hear '(.*)' echoed back")]
+        public void ThenIHearEchoedBack(string exclamation)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
